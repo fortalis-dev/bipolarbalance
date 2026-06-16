0055b700c8a262685711ccff1c39847588ecd732
Nils <fortalis@proton.me>
2026-06-16 12:54:00 +0200

    Bump version to 4.0 (4) and disable minification for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,15 +10,15 @@ android {
         applicationId = "com.bipolar.balance"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
